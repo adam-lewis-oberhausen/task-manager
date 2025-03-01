a7c8c39af25065bcf733599691fa3420035e6752
adam-lewis-oberhausen <aoberhau@gmail.com>
2025-02-28 23:00:22 -0500

    missed some files

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors()); // Add this line
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
