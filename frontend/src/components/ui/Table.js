dc89355e2806f44ce4aa9b9abf34933f4cd3257d
adam-lewis-oberhausen <aoberhau@gmail.com>
2025-03-22 20:37:45 -0400

    All unit test for TaskRow implemented.

--- frontend/src/components/ui/Table.js
+++ frontend/src/components/ui/Table.js
@@ -21,13 +21,22 @@ const TableRow = ({ children, className = '' }) => (
   <tr className={`${styles.tableRow} ${className}`}>{children}</tr>
 );
 
-const TableCell = ({ children, className = '', label = '', onClick, onMouseEnter, onMouseLeave }) => (
-  <td 
+const TableCell = ({
+  children,
+  className = '',
+  label = '',
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+  'aria-label': ariaLabel
+}) => (
+  <td
     className={`${styles.tableCell} ${className}`}
     data-label={label}
     onClick={onClick}
     onMouseEnter={onMouseEnter}
     onMouseLeave={onMouseLeave}
+    aria-label={ariaLabel}
   >
     {children}
   </td>
